c9f563105f64534cea9c0ae7d9ca7faa7dfd80b7
Auro Tripathy <tripathy_a@auro-mbp.attlocal.net>
2016-10-25 11:03:37 -0700

    Initial Commit

---
+++ AVCam/AVCamPhotoCaptureDelegate.m
@@ -0,0 +1,228 @@
+/*
+	Copyright (C) 2016 Apple Inc. All Rights Reserved.
+	See LICENSE.txt for this sample’s licensing information
+	
+	Abstract:
+	Photo capture delegate.
+*/
+
+#import "AVCamPhotoCaptureDelegate.h"
+
+@import Photos;
+
+@interface AVCamPhotoCaptureDelegate ()
+
+@property (nonatomic, readwrite) AVCapturePhotoSettings *requestedPhotoSettings;
+@property (nonatomic) void (^willCapturePhotoAnimation)();
+@property (nonatomic) void (^capturingLivePhoto)(BOOL capturing);
+@property (nonatomic) void (^completed)(AVCamPhotoCaptureDelegate *photoCaptureDelegate);
+
+@property (nonatomic) NSData *photoData;
+@property (nonatomic) NSURL *livePhotoCompanionMovieURL;
+
+@end
+
+//GLOBALS
+NSMutableURLRequest *request;
+
+@implementation AVCamPhotoCaptureDelegate
+
+- (instancetype)initWithRequestedPhotoSettings:(AVCapturePhotoSettings *)requestedPhotoSettings willCapturePhotoAnimation:(void (^)())willCapturePhotoAnimation capturingLivePhoto:(void (^)(BOOL))capturingLivePhoto completed:(void (^)(AVCamPhotoCaptureDelegate *))completed
+{
+	self = [super init];
+	if ( self ) {
+		self.requestedPhotoSettings = requestedPhotoSettings;
+		self.willCapturePhotoAnimation = willCapturePhotoAnimation;
+		self.capturingLivePhoto = capturingLivePhoto;
+		self.completed = completed;
+	}
+	return self;
+}
+
+- (void)didFinish
+{
+	if ( [[NSFileManager defaultManager] fileExistsAtPath:self.livePhotoCompanionMovieURL.path] ) {
+		NSError *error = nil;
+		[[NSFileManager defaultManager] removeItemAtPath:self.livePhotoCompanionMovieURL.path error:&error];
+		
+		if ( error ) {
+			NSLog( @"Could not remove file at url: %@", self.livePhotoCompanionMovieURL.path );
+		}
+	}
+	
+	self.completed( self );
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput willBeginCaptureForResolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings
+{
+	if ( ( resolvedSettings.livePhotoMovieDimensions.width > 0 ) && ( resolvedSettings.livePhotoMovieDimensions.height > 0 ) ) {
+		self.capturingLivePhoto( YES );
+	}
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput willCapturePhotoForResolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings
+{
+	self.willCapturePhotoAnimation();
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput didFinishProcessingPhotoSampleBuffer:(CMSampleBufferRef)photoSampleBuffer previewPhotoSampleBuffer:(CMSampleBufferRef)previewPhotoSampleBuffer resolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings bracketSettings:(AVCaptureBracketedStillImageSettings *)bracketSettings error:(NSError *)error
+{
+	if ( error != nil ) {
+		NSLog( @"Error capturing photo: %@", error );
+		return;
+	}
+	
+	self.photoData = [AVCapturePhotoOutput JPEGPhotoDataRepresentationForJPEGSampleBuffer:photoSampleBuffer previewPhotoSampleBuffer:previewPhotoSampleBuffer];
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput didFinishRecordingLivePhotoMovieForEventualFileAtURL:(NSURL *)outputFileURL resolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings
+{
+	self.capturingLivePhoto(NO);
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput didFinishProcessingLivePhotoToMovieFileAtURL:(NSURL *)outputFileURL duration:(CMTime)duration photoDisplayTime:(CMTime)photoDisplayTime resolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings error:(NSError *)error
+{
+	if ( error != nil ) {
+		NSLog( @"Error processing live photo companion movie: %@", error );
+		return;
+	}
+	
+	self.livePhotoCompanionMovieURL = outputFileURL;
+}
+
+- (UIImage*) imageWithImage: (UIImage*) sourceImage : (float) i_width
+{
+    float oldWidth = sourceImage.size.width;
+    float scaleFactor = i_width / oldWidth;
+    
+    float newHeight = sourceImage.size.height * scaleFactor;
+    float newWidth = oldWidth * scaleFactor;
+    
+    UIGraphicsBeginImageContext(CGSizeMake(newWidth, newHeight));
+    [sourceImage drawInRect:CGRectMake(0, 0, newWidth, newHeight)];
+    UIImage *newImage = UIGraphicsGetImageFromCurrentImageContext();
+    UIGraphicsEndImageContext();
+    return newImage;
+}
+
+-(BOOL) setPostParams:(NSData *)imageData{
+    
+    
+#define URL            @"https://88462519.ngrok.io/curl/"  // change this URL when the server moves
+    
+    if(self.photoData != nil) {
+        request = [NSMutableURLRequest new];
+        request.timeoutInterval = 20.0;
+        [request setURL:[NSURL URLWithString:URL]];
+        [request setHTTPMethod:@"POST"];
+        
+        NSString *boundary = @"---------------------------14737809831466499882746641449";
+        NSString *contentType = [NSString stringWithFormat:@"multipart/form-data; boundary=%@",boundary];
+        [request addValue:contentType forHTTPHeaderField: @"Content-Type"];
+        
+        
+        
+        [request setValue:@"text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8" forHTTPHeaderField:@"Accept"];
+        [request setValue:@"Mozilla/5.0 (Macintosh; Intel Mac OS X 10_7_5) AppleWebKit/536.26.14 (KHTML, like Gecko) Version/6.0.1 Safari/536.26.14" forHTTPHeaderField:@"User-Agent"];
+        
+        NSMutableData *body = [NSMutableData data];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@\r\n",boundary]
+                          dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        [body appendData:[[NSString stringWithFormat:@"Content-Disposition: form-data; name=\"file\"; filename=\"%@.jpg\"\r\n", @"Uploaded_file"]   dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        //auro
+        // TODO should the mime type be image/jpeg
+        // [body appendData:[@"Content-Type: image/jpeg" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[@"Content-Type: application/octet-stream\r\n\r\n" dataUsingEncoding:NSUTF8StringEncoding]];
+        [body appendData:[NSData dataWithData:imageData]];
+        [body appendData:[[NSString stringWithFormat:@"\r\n--%@--\r\n",boundary] dataUsingEncoding:NSUTF8StringEncoding]];
+        
+        
+        [request setHTTPBody:body];
+        [request addValue:[NSString stringWithFormat:@"%lu", (unsigned long)[body length]] forHTTPHeaderField:@"Content-Length"];
+        
+        NSLog(@"HTTP Content Length = %lu", (unsigned long)body.length);
+        
+        return TRUE;
+        
+    }else{
+        return FALSE;
+    }
+}
+
+- (void)captureOutput:(AVCapturePhotoOutput *)captureOutput didFinishCaptureForResolvedSettings:(AVCaptureResolvedPhotoSettings *)resolvedSettings error:(NSError *)error
+{
+	if ( error != nil ) {
+		NSLog( @"Error capturing photo: %@", error );
+		[self didFinish];
+		return;
+	}
+	
+	if ( self.photoData == nil ) {
+		NSLog( @"No photo data resource" );
+		[self didFinish];
+		return;
+	}
+	//auro another place to ship out before writing photoData
+    //raw data i'm presuming
+    UIImage *image = [[UIImage alloc] initWithData:self.photoData];
+    int heightInPoints = image.size.height;
+    int heightInPixels = heightInPoints * image.scale;
+    
+    int widthInPoints = image.size.width;
+    int widthInPixels = widthInPoints * image.scale;
+    NSLog(@"width height %d %d", widthInPixels, heightInPixels);
+    
+    //scale it down
+    image = [self imageWithImage:image : 400];
+
+    heightInPoints = image.size.height;
+    heightInPixels = heightInPoints * image.scale;
+    
+    widthInPoints = image.size.width;
+    widthInPixels = widthInPoints * image.scale;
+    NSLog(@"scaled width height %d %d", widthInPixels, heightInPixels);
+    
+    NSData *scaled_imagedata = UIImageJPEGRepresentation(image, 90);
+    [self setPostParams:scaled_imagedata];
+    
+    NSError *http_error = nil;
+    NSURLResponse *responseStr = nil;
+    NSData *syncResData;
+    syncResData = [NSURLConnection sendSynchronousRequest:request returningResponse:&responseStr error:&http_error];
+    NSString *returnString = [[NSString alloc] initWithData:syncResData encoding:NSUTF8StringEncoding];
+    
+    NSLog(@"ERROR %@", http_error);
+    NSLog(@"RES %@", responseStr);
+    
+    NSLog(@"RETURN STRING:%@", returnString);
+    
+    
+    [PHPhotoLibrary requestAuthorization:^( PHAuthorizationStatus status ) {
+		if ( status == PHAuthorizationStatusAuthorized ) {
+			[[PHPhotoLibrary sharedPhotoLibrary] performChanges:^{
+				PHAssetCreationRequest *creationRequest = [PHAssetCreationRequest creationRequestForAsset];
+				[creationRequest addResourceWithType:PHAssetResourceTypePhoto data:self.photoData options:nil];
+				
+				if ( self.livePhotoCompanionMovieURL ) {
+					PHAssetResourceCreationOptions *livePhotoCompanionMovieResourceOptions = [[PHAssetResourceCreationOptions alloc] init];
+					livePhotoCompanionMovieResourceOptions.shouldMoveFile = YES;
+					[creationRequest addResourceWithType:PHAssetResourceTypePairedVideo fileURL:self.livePhotoCompanionMovieURL options:livePhotoCompanionMovieResourceOptions];
+				}
+			} completionHandler:^( BOOL success, NSError * _Nullable error ) {
+				if ( ! success ) {
+					NSLog( @"Error occurred while saving photo to photo library: %@", error );
+				}
+				
+				[self didFinish];
+			}];
+		}
+		else {
+			NSLog( @"Not authorized to save photo" );
+			[self didFinish];
+		}
+	}];
+}
+
+@end
